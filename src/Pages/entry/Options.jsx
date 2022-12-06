27e0c0b9a105770f9204c920a18bcf0cf128b8a2
a.shamsollahi <a.shamsollahi@citydi.com>
2022-12-06 13:56:00 +0330

    code and test scoop option component

---
+++ src/Pages/entry/Options.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((error) => {});
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoop" ? ScoopOption : null;
+
+  const OptionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{OptionItems}</Row>;
+}
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,11 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoop", (req, res, ctx) => {
     return res(
       ctx.json([
         { name: "Chocolate", imagepath: "/images/chocolate.png" },
-        { name: "Vanila", imagepath: "/images/vanila.png" },
+        { name: "Vanilla", imagepath: "/images/vanila.png" },
       ])
     );
   }),
